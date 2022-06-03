7af2adabd5cbb482e2050c13a9670e7288aa94f6
Akilesh <akileshakileshs1234@gmail.com>
2022-06-03 09:34:15 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,25 @@
+package com.akilesh;
+
+public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+        boolean isParameterInRange = ((year >= 1) && (year <= 9999)) ? true : false;
+        if(!isParameterInRange) {
+            return false;
+        } else {
+            if((year % 4) == 0) {
+                if((year % 100) == 0) {
+                    if((year % 400) == 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+}
